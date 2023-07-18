8dae152570bdd9e6fb350046e1ccce3a01c2e963
RubinaSapra <45000373+RubinaSapra@users.noreply.github.com>
2023-07-18 13:03:20 +0530

    Create PythonCode.py

---
+++ JenkinFile/PythonCode.py
@@ -0,0 +1,17 @@
+import os
+list1 = [8,5,2,1]
+list1.reverse()
+print("Rev list",list1)
+
+list2= [91,67,81,23,43]
+list2.sort()
+print("Sort list",list2)
+
+list3= []
+list3= list3.copy()
+print("Copied list",list3)
+
+indexvalue = list2[2:5]
+print("Index valu"=indexvalue)
+
+
